8fe97ec28c19087e6527b739f38598c6c6f7b0af
root <nverchok@ucsd.edu>
2020-01-20 20:51:57 -0800

    slightly more help in copyCgram in displayBuffer

--- displayBuffer.c
+++ displayBuffer.c
@@ -9,7 +9,16 @@ char displayBuffer[MAXHEIGHT][MAXWIDTH];
  * cGram into the displayBuffer starting at column xPos. Returns the
  * column where the next cGram should start. */
 int copyCGram(const char c, int xPos) {
-	// TODO
+	
+	// TODO:
+	//   - lookup the start of the specified char's cGram
+	//   - iterate over all of the cGram's symbols (when do you stop?)
+	//   - copy its symbols into displayBuffer (when do you advance
+	//     along the columns and when do you go down the rows?)
+	//   - return the column of displayBuffer where *the next* cGram
+	//     should start, accounting for the width of the current cGram
+	//     and the extra offset given by the global SPACING variable
+
 }
 
 
